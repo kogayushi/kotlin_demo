02de2655ec3b1076f9b48635cd5fc5b4ce4ce79c
yushi koga <yushi.koga@is-tech.co.jp>
2019-07-11 21:40:07 +0900

    page 219

--- src/main/kotlin/com/example/demo/TaskController.kt
+++ src/main/kotlin/com/example/demo/TaskController.kt
@@ -2,7 +2,10 @@ package com.example.demo
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -19,5 +22,17 @@ class TaskController(private val taskRepository: TaskRepository) {
     @GetMapping("new")
     fun new(form: TaskCreateForm): String {
         return "tasks/new"
+    }
+
+    @PostMapping
+    fun create(@Validated form: TaskCreateForm,
+               bindingResult: BindingResult): String {
+        if(bindingResult.hasErrors()) {
+            return "tasks/new"
+        }
+
+        val content = requireNotNull(form.content)
+        taskRepository.create(content)
+        return "redirect:/tasks"
     }
 }
