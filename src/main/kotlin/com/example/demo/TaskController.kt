5916acf3b25d098ec89dba235d8970f31e67298e
yushi koga <yushi.koga@is-tech.co.jp>
2019-07-11 21:49:36 +0900

    page 223

--- src/main/kotlin/com/example/demo/TaskController.kt
+++ src/main/kotlin/com/example/demo/TaskController.kt
@@ -1,6 +1,5 @@
 package com.example.demo
 
-import javassist.NotFoundException
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
